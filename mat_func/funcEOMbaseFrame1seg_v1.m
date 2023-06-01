a0d4289d42436e7ee460259fee6cc599642e3963
zhiqiao0256 <37387088+zhiqiao0256@users.noreply.github.com>
2023-06-01 11:42:18 -0700

    eom fix

--- mat_func/funcEOMbaseFrame1seg_v1.m
+++ mat_func/funcEOMbaseFrame1seg_v1.m
@@ -1,182 +1,110 @@
 function par =funcEOMbaseFrame1seg_v1(par)
 fprintf( 'EOM... \n' )
-%% Transformations
-par.n= 3;%DOF
-%%% symbolic variables
-syms m0 g h0 lc1 lc2 
-xi = sym('xi', [par.n 1]);
-dxi = sym('dxi', [par.n 1]);
-Iyyi = sym('Iyy',[par.n 1]);
+n = 6; % DOF
 
-rigid_r=sym(zeros(1,par.n));
-rigid_alpha=sym(zeros(1,par.n));%alpha
-rigid_d=sym(zeros(1,par.n));    
-rigid_theta=sym(zeros(1,par.n));
-rigid_m = sym(zeros(1,par.n));
-%%% x pointing left side 
-%%% DH talbe %%%
-%%% Link    theta   d     alpha   a   
-%%% 1       0       xi(1)  pi/2   0   
-%%% 2       xi(2)   0      -pi/2   
-%%% 3       0       xi(3)  0   0
+% DH parameters symbols
+q = sym('q', [n 1], 'real'); % generalized coordinates (joint angles)
+qd = sym('qd', [n 1], 'real');
+Iyyi = sym('Iyy',[n 1],'real');
+syms a1 g m1 m2 real
 
-rigid_alpha(1)= pi/2;
-rigid_alpha(2)= -pi/2;
+T0i = cell(n,1);
 
-rigid_d(1)= xi(1);
-rigid_d(3)= xi(3);
+DH = [0 -pi/2 q(1)  0;
+      0  pi/2   0   q(2); 
+      0     0 q(3)  0;
+      0 -pi/2 q(4)+a1  0;
+      0  pi/2   0   q(5); 
+      0     0 q(6)  0;];
 
-rigid_theta(2)= xi(2);
-
-rigid_m(1)= 0;
-rigid_m(2)= m0;
-rigid_m(3)= 0;
-
-% n=length(q);% DOF
-% cell array of your homogeneous transformations; each Ti{i} is a 4x4 symbolic transform matrix
-T_old_to_i = cell(par.n,1);% z0 z_end_effector
-Ti_from_0 = cell(par.n+1,1);
-pi_from_0 = cell(par.n+1,1);
-zi_from_0 = cell(par.n+1,1);
-% Ti(1) = {[1 0 0 0;0 1 0 0; 0 0 1 0; 0 0 0 1]};
-for i =1:length(rigid_m)
-    T_old_to_i{i} = [cos(rigid_theta(i)), -sin(rigid_theta(i)) * cos(rigid_alpha(i)), sin(rigid_theta(i)) * sin(rigid_alpha(i)), rigid_r(i) * cos(rigid_theta(i));...
-                     sin(rigid_theta(i)),  cos(rigid_theta(i)) * cos(rigid_alpha(i)), -cos(rigid_theta(i)) * sin(rigid_alpha(i)), rigid_r(i) * sin(rigid_theta(i));...
-                     0, sin(rigid_alpha(i)), cos(rigid_alpha(i)),rigid_d(i);...
-                     0, 0, 0, 1   ];
+T = eye(4);
+for i = 1:n
+    temp = compute_dh_matrix(DH(i,1),DH(i,2),DH(i,3),DH(i,4));
+    T = T*temp;
+    T0i{i} = T;
+    zi_from_0{i} = T(1:3,3);
+    pi_from_0{i} = T(1:3,4);
 end
-Ti_from_0{1} = eye(4);
-pi_from_0{1}=Ti_from_0{1}(1:3,4);
-zi_from_0{1}=Ti_from_0{1}(1:3,3);
-for i = 2:length(Ti_from_0)
-    Ti_from_0{i} =  Ti_from_0{i-1} * T_old_to_i{i-1};
-    pi_from_0{i}=Ti_from_0{i}(1:3,4);
-    zi_from_0{i}=Ti_from_0{i}(1:3,3);
+%% Jacobian
+Jv = cell(n,1);
+P = [];
+for i=1:n    
+P = T0i{i};
+    x = P(1,4);
+    y = P(2,4);
+    z = P(3,4);
+    for j=1:n
+        Jv{i} = [Jv{i} [diff(x,q(j));diff(y,q(j));diff(z,q(j))]];
+    end        
 end
-par.Ti=Ti_from_0;
-fprintf( 'Ep.. \n' )
-par.rigid_2_htm = Ti_from_0{4};
-par.rigid_3_htm = Ti_from_0{end};
-% return
-%% Protential energy
-E_p=0;
-for link_i=1:length(rigid_r)
-    E_p=E_p+rigid_m(link_i)*[0;0;g].'*pi_from_0{link_i+1};
-end
-fprintf( 'J_v... \n' )
-par.Ep = E_p;
-%% Linear Velocity
-J_v=cell(par.n,1);
-for link_i = 1:par.n
-    j_v=sym(zeros(3,par.n));
-    for j_counter =1:link_i
-        if rigid_theta(j_counter) == 0 %% Prismatic Joint
-            j_v(:,j_counter)=zi_from_0{j_counter};
+Jw=cell(n,1);
+for i= 1:n
+    jw=sym(zeros(3,n));
+    for j_counter =1:i
+        if DH(j_counter,4) == 0 %% Prismatic Joint
+            jw(:,j_counter)=zeros(3,1);
         else %% Rotational Joint
-            j_v(:,j_counter)=cross(zi_from_0{j_counter},(pi_from_0{link_i+1}-pi_from_0{j_counter}));
+            jw(:,j_counter)=zi_from_0{j_counter};
         end
     end
-    J_v{link_i}=j_v;
+    Jw{i}=jw;
 end
-fprintf( 'J_w... \n' )
-% return
-
-J_w=cell(par.n,1);
-for link_i= 1:par.n
-    j_w=sym(zeros(3,par.n));
-    for j_counter =1:link_i
-        if rigid_theta(j_counter) == 0 %% Prismatic Joint
-            j_w(:,j_counter)=zeros(3,1);
-        else %% Rotational Joint
-            j_w(:,j_counter)=zi_from_0{j_counter};
-        end
-    end
-    J_w{link_i}=j_w;
-end
-% par.Jw=J_w;
-%% Unite Jacobian
-par.J_xyz=cell(par.n,1);
-for i =1:length(J_w)
-    par.J_xyz{i}=[J_v{i};J_w{i}];
-    
-end
-par.sym_J_xyz2xi=par.J_xyz{end};    
-fprintf( 'D... \n' )
-
-%% Inerial and Kinetic energy
-I=cell(par.n,1);
-for link_i =1:par.n
-    if rigid_m(link_i) == 0 % no mass
-        I{link_i}=zeros(3,3);
+%% Protential energy and Kinetic 
+m = sym(zeros(n,1));
+m(2) = m1;
+m(5) = m2;
+PE = 0;
+D = 0;
+I=cell(n,1);
+for i =1:n
+    if m(i) == 0 % no mass
+        I{i}=zeros(3,3);
     else
 %         I{link_i}=[Ixx Ixy Ixz;
 %            Ixy Iyy Iyz;
 %            Ixz Iyz Izz];
-        I{link_i}= Iyyi(link_i);
+        I{i}= Iyyi(i);
 
     end
 end
-D = (rigid_m(1)*J_v{1}.'*J_v{1} + J_w{1}.'*I{1}*J_w{1});
 
-for d_counter = 2:par.n
-%     Di_v = rigid_m(d_counter)*J_v{d_counter}.'*J_v{d_counter}
-%     Di_w = J_w{d_counter}.'*I{d_counter}*J_w{d_counter}
-    D = D + (rigid_m(d_counter)*J_v{d_counter}.'*J_v{d_counter} + J_w{d_counter}.'*I{d_counter}*J_w{d_counter});
-%     d_counter
+for i=1:n
+    P = T0i{i};
+    PE=PE+m(i)*g*P(3,4);
+    D = D + (m(i)*Jv{i}'*Jv{i} + Jw{i}'*I{i}*Jw{i});
 end
-fprintf( 'C... \n' )
-%% Coriolis
-Cs = sym(zeros(par.n,par.n,par.n));
-for i1 = 1:par.n
-    for j1 = 1:par.n
-        for k1 = 1:par.n
-              diff1 = 1/2*(diff(D(k1,j1),xi(i1)));
-            diff2 = 1/2*(diff(D(k1,i1),xi(j1)));
-            diff3 = 1/2*(diff(D(i1,j1),xi(k1)));
-            Cs(i1,j1,k1) = (diff1+diff2-diff3)*dxi(i1);
+fprintf( 'J_v... \n' )
+par.Ep = PE;
+%% 
+c = zeros(n,n,n,'sym');
+for k = 1:n
+    for i = 1:n
+        for j =1:n
+            c(i,j,k) = 0.5 * (diff(D(k,j),q(i)) + diff(D(k,i),q(j)) - diff(D(i,j),q(k)));
         end
     end
 end
-cor = sym(zeros(par.n,par.n));
-fprintf( 'cor... \n' )
-for k1 = 1:par.n
-    for j1 = 1:par.n 
-        for i1 = 1:par.n
-            cor(k1,j1)=cor(k1,j1)+Cs(i1, k1 , j1);
+
+% The coriolis matrix
+C = zeros(n,n,'sym');
+for k = 1:n
+    for j = 1:n
+        temp = 0;
+        for i = 1:n
+            temp = temp + c(i,j,k)*qd(i);
         end
+        C(j,k) = temp;
     end
 end
-Phi = sym(zeros(par.n,1));
-
-for i1 = 1:par.n
-    Phi(i1) = diff(E_p,xi(i1));
-%     Phi = Phi;2345
+G = zeros(n,1,'sym');
+for k = 1:n
+    G(k) = diff(PE,q(k));
 end
-%% EOM rigid
-% syms f_p1 f_p2 f_p3
-% ddxi = sym('ddxi', [par.n 1], 'rational'); % "q double dot" - the second derivative of the q's in time (joint accelerations)
-% eom_lhs = D*ddxi+cor*dxi+Phi;
 
 par.B_rigid=D;
-par.C_rigid=cor;
-par.G_rigid=Phi;
-return
-par.xi_0_G_rigid=subs(Phi,[xi(2),xi(5)],[0,0]);
-par.xi2_0_G_rigid=subs(Phi,[xi(2)],[0]);
-par.xi5_0_G_rigid=subs(Phi,[xi(5)],[0]);
+par.C_rigid=C;
+par.G_rigid=G;
 
-% for i =1:3
-%     T_p{i}=Ti{end}*[eye(3),par.r_p{i};0 0 0 1];
-%     r_p_base{i}=T_p{i}(1:3,4);
-% end
-% % par.sym_wrench=[f_p1*T_p{1}(1:3,3);cross(r_p_base{1},f_p1*T_p{1}(1:3,3))]+...
-% %     [f_p2*T_p{2}(1:3,3);cross(r_p_base{2},f_p2*T_p{2}(1:3,3))]+...
-% %     [f_p3*T_p{3}(1:3,3);cross(r_p_base{3},f_p3*T_p{3}(1:3,3))];
-% % 
-% % eom_rhs=par.J_xyz{end}.'*par.sym_wrench;
-% % par.f_xi=eom_rhs;
-% % % par.sym_wrench=[f_x f_y f_z tau_x tau_y tau_z].';
-% return
 %% mapping
 
 syms theta1 dtheta1 ddtheta1 theta1_t(t) lc1 dlc1 ddlc1 lc1_t(t)
@@ -187,30 +115,25 @@ b_theta2 = lc2/(theta2)*tan(theta2/2);
 m_q=[b_theta1 theta1 b_theta1 b_theta2 theta2 b_theta2 ].';% 6x1
 J_f=[diff(m_q,theta1),diff(m_q,lc1),diff(m_q,theta2),diff(m_q,lc2)];%6x4
 
-temp.dJ_f=diff(subs(J_f,[theta1,lc1,theta2,lc2],[theta1_t(t),lc1_t(t),theta2_t(t),lc2_t(t)]),t);%6x4
-dJ_fdt=subs(temp.dJ_f,[theta1_t(t),diff(theta1_t(t),t),lc1_t(t),diff(lc1_t(t),t),theta2_t(t),diff(theta2_t(t),t),lc2_t(t),diff(lc2_t(t), t)]...
+
+temp_dJ_f=diff(subs(J_f,[theta1,lc1,theta2,lc2],...
+    [theta1_t(t),lc1_t(t),theta2_t(t),lc2_t(t)]),t);%6x4
+dJ_fdt=subs(temp_dJ_f,[theta1_t(t),diff(theta1_t(t),t),lc1_t(t),diff(lc1_t(t),t),theta2_t(t),diff(theta2_t(t),t),lc2_t(t),diff(lc2_t(t), t)]...
     ,[theta1,dtheta1,lc1,dlc1,theta2,dtheta2,lc2,dlc2]);% 6x4
 par.J_xi2q=J_f;%6x4
-temp.xi=m_q;%6x1
-temp.dxi=J_f*[dtheta1 dlc1 dtheta2 dlc2].'; %6x4 * 4x1
-temp.ddxi=dJ_fdt*[dtheta1 dlc1 dtheta2 dlc2].'+J_f*[ddtheta1 ddlc1 ddtheta2 ddlc2].';% 6x4 * 4x1 + 6x4 * 4x1
+temp_xi=m_q;%6x1
+temp_dxi=J_f*[dtheta1 dlc1 dtheta2 dlc2].'; %6x4 * 4x1
+temp_ddxi=dJ_fdt*[dtheta1 dlc1 dtheta2 dlc2].'+J_f*[ddtheta1 ddlc1 ddtheta2 ddlc2].';% 6x4 * 4x1 + 6x4 * 4x1
 % %%
-B_xi_q=subs(D,xi,m_q);
-par.sym_J_xi2q=subs(par.J_xyz{end},xi,m_q);
+B_xi_q=subs(D,q,m_q);
+% par.sym_J_xi2q=subs(par.J_xyz{end},xi,m_q);
 B_q=J_f.'*B_xi_q*J_f;
-%%%
-% M_xi_q=subs(M,xi,m_q);
-% par.B_q_simplify=J_f.'*M_xi_q*J_f;
-% %%
-% temp_1=subs(cor,xi,f);
-% temp_2=subs(temp_1,dxi,df);
-% %%
-% C_q=J_f.'*subs(D,xi,f)*J_ff+J_f.'*temp_2*J_f;
+
 fprintf( 'cq... \n' )
-C_q=J_f.'*subs(D,xi,m_q)*dJ_fdt+J_f.'*subs(cor,[xi;dxi],[temp.xi;temp.dxi])*J_f;
+C_q=J_f.'*subs(D,q,m_q)*dJ_fdt+J_f.'*subs(C,[q;qd],[temp_xi;temp_dxi])*J_f;
 % %%
 fprintf( 'g_q... \n' )
-G_q=J_f.'*subs(Phi,xi,m_q);
+G_q=J_f.'*subs(G,q,m_q);
 % %%
 % par.J_xyz2q=subs(par.J_xyz2xi*par.J_xi2q,[xi],[m_q]);
 
@@ -221,60 +144,7 @@ G_q=J_f.'*subs(Phi,xi,m_q);
 par.B_q=B_q;
 par.C_q=C_q;
 par.G_q=G_q;
-%% theta = 0
 
-b_theta1 = lc1/2;
-b_theta2 = lc2/2;
-
-m_q=[b_theta1 theta1 b_theta1 b_theta2 theta2 b_theta2 ].';% 6x1
-J_f=[diff(m_q,theta1),diff(m_q,lc1),diff(m_q,theta2),diff(m_q,lc2)];%6x4
-
-temp.dJ_f=diff(subs(J_f,[theta1,lc1,theta2,lc2],[theta1_t(t),lc1_t(t),theta2_t(t),lc2_t(t)]),t);%6x4
-dJ_fdt=subs(temp.dJ_f,[theta1_t(t),diff(theta1_t(t),t),lc1_t(t),diff(lc1_t(t),t),theta2_t(t),diff(theta2_t(t),t),lc2_t(t),diff(lc2_t(t), t)]...
-    ,[theta1,dtheta1,lc1,dlc1,theta2,dtheta2,lc2,dlc2]);% 6x4
-par.J_xi2q=J_f;%6x4
-temp.xi=m_q;%6x1
-temp.dxi=J_f*[dtheta1 dlc1 dtheta2 dlc2].'; %6x4 * 4x1
-temp.ddxi=dJ_fdt*[dtheta1 dlc1 dtheta2 dlc2].'+J_f*[ddtheta1 ddlc1 ddtheta2 ddlc2].';% 6x4 * 4x1 + 6x4 * 4x1
-% %%
-B_xi_q=subs(D,xi,m_q);
-par.theta0_sym_J_xi2q=subs(par.J_xyz{end},xi,m_q);
-B_q=J_f.'*B_xi_q*J_f;
-%%%
-% M_xi_q=subs(M,xi,m_q);
-% par.B_q_simplify=J_f.'*M_xi_q*J_f;
-% %%
-% temp_1=subs(cor,xi,f);
-% temp_2=subs(temp_1,dxi,df);
-% %%
-% C_q=J_f.'*subs(D,xi,f)*J_ff+J_f.'*temp_2*J_f;
-fprintf( 'cq... \n' )
-C_q=J_f.'*subs(D,xi,m_q)*dJ_fdt+J_f.'*subs(cor,[xi;dxi],[temp.xi;temp.dxi])*J_f;
-% %%
-fprintf( 'g_q... \n' )
-G_q=J_f.'*subs(Phi,xi,m_q);
-% subs(G_q.[theta1,theta2],[0,0])
-% %%
-% par.J_xyz2q=subs(par.J_xyz2xi*par.J_xi2q,[xi],[m_q]);
-
-% f_q=J_f.'*subs(par.f_xi,xi,f);
-% 
-% %%
-% % par={};
-par.theta_0_B_q=subs(B_q,[theta1,theta2],[0,0]);
-par.theta_0_C_q=subs(C_q,[theta1,theta2],[0,0]);
-par.theta_0_G_q=subs(G_q,[theta1,theta2],[0,0]);
-
-par.theta1_0_B_q=subs(B_q,[theta1],[0]);
-par.theta1_0_C_q=subs(C_q,[theta1],[0]);
-par.theta1_0_G_q=subs(G_q,[theta1],[0]);
-
-
-par.theta2_0_B_q=subs(B_q,[theta2],[0]);
-par.theta2_0_C_q=subs(C_q,[theta2],[0]);
-par.theta2_0_G_q=subs(G_q,[theta2],[0]);
-% % par.C_q_simplify=simplify(C_q);
-% % par.G_q_simplify=simplify(G_q);
 %% Actuation mapping
 fprintf('EOM Done\n')
 end
--- mat_func/funcGitTest.m
+++ mat_func/funcGitTest.m
@@ -3,19 +3,18 @@ n = 6; % DOF
 
 % DH parameters symbols
 q = sym('q', [n 1], 'real'); % generalized coordinates (joint angles)
-d = sym('d', [n 1], 'real'); % link offsets
 syms a1
 syms g
 
 Ti = cell(n,1);
 
-DH = [0 -pi/2 d(1)  0;
+DH = [0 -pi/2 q(1)  0;
       0  pi/2   0   q(2); 
-      0     0 d(3)  0;
+      0     0 q(3)  0;
       0     0 a1  0;
-      0 -pi/2 d(4)  0;
+      0 -pi/2 q(4)  0;
       0  pi/2   0   q(5); 
-      0     0 d(6)  0;];
+      0     0 q(6)  0;];
 T = eye(4);
 for i = 1:n
     temp = compute_dh_matrix(DH(i,1),DH(i,2),DH(i,3),DH(i,4));
@@ -27,20 +26,18 @@ par.Ti = Ti;
 Jw = arrayfun(@(x) sym(['Jw' num2str(x)], [3,n], 'real'), 1:n, 'UniformOutput', 0)';
 Jw{1} = [[0;0;1] repmat([0;0;0],1,n-1)];
 for i=2:n
-       jw = [[0;0;1]];
+       jw = [0;0;1];
        for j=1:i-1
            jw = [jw Ti{j}(1:3,3)];
        end
        jw = [jw repmat([0;0;0],1,n-i)];
        Jw{i} =jw;
 end
-c = arrayfun(@(x) [sym(['c' num2str(x) 'x'], 'real'), sym(['c' num2str(x) 'y'], 'real'), ...
-    sym(['c' num2str(x) 'z'], 'real')]', 1:n, 'UniformOutput', 0)'
-
+par.Jw =Jw;
 Jv = cell(n,1);
 P = eye(4);
 for i=1:n    
-P = Ti{i}*[[1;0;0;0] [0;1;0;0] [0;0;1;0] [c{i};1]];
+P = Ti{i};
     x = P(1,4);
     y = P(2,4);
     z = P(3,4);
@@ -49,4 +46,5 @@ P = Ti{i}*[[1;0;0;0] [0;1;0;0] [0;0;1;0] [c{i};1]];
     end        
 end
 m = sym('m', [n 1], 'real');
-par.
+par.Pe = P;
+par.Jv = Jv;
